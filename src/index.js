b8c2e2dc6449e323a3e5104b391af2701254cb93
md-mannan <mdmannan580@gmail.com>
2022-10-18 19:48:55 +0300

    JS Dev Boilerplate

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+console.log("hello world!");
+
+function greet(name){
+    console.log('Good morning  '+name)
+}
+
+greet('M A Mannan')
+greet("H M Nayem")
+greet("H M Mannan")
+
+
+class X{
+    a=60
+    ABC(a){
+        console.log(a)
+    }
+}
+let a=new X()
+a.ABC(50)
+a.ABC(50000)
